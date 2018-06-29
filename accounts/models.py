f9a4e0b1a67807f9db0be8b8516aded65ad9dc78
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-06-29 10:56:35 +0100

    Profile model created and migrated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 from tinymce.models import HTMLField
+
+
+class Profile(models.Model):
+    """User profile Model
+    :Inherit: - models.Model
+    bio - use HTMLField from tinymce.models
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    email = models.EmailField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zip = models.IntegerField()
+    bio = HTMLField()
+    avatar = models.ImageField(upload_to='user_profile_avatar')
+    hobbies = models.CharField(max_length=255, blank=True)
+    favorite_animals = models.CharField(max_length=255, blank=True)
+    user = models.OneToOneField(User, related_name='profile')
+
+
