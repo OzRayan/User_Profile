1de5f8f3e9c1e1f401ff3b33e3c64f734d2ea876
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-06-29 09:55:58 +0100

    update

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,3 @@
 from django.contrib.auth.models import User
 from django.db import models
 from tinymce.models import HTMLField
-
-
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,13 +5,13 @@ from . import views
 urlpatterns = [
     url(r'sign_in/$', views.sign_in, name='sign_in'),
     url(r'sign_up/$', views.sign_up, name='sign_up'),
-    url(r'sign_out/$', views.sign_out, name='sign_out'),
-    url(r'profile//$', views.,
-        name=''),
-    url(r'profile/view/$', views.,
-        name='user_profile_view'),
-    url(r'profile//$', views.,
-        name='change_password'),
-    url(r'profile//$', views.,
-        name='edit_avatar')
-]
+    url(r'sign_out/$', views.sign_out, name='sign_out'),]
+#     url(r'profile//$', views.,
+#         name=''),
+#     url(r'profile/view/$', views.,
+#         name=''),
+#     url(r'profile//$', views.,
+#         name=''),
+#     url(r'profile//$', views.,
+#         name='')
+# ]
