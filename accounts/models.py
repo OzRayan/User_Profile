3773f29dd6fb3f1b1560a26901e2bb005492098b
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2019-02-28 21:56:37 +0100

    update

--- accounts/models.py
+++ accounts/models.py
@@ -20,5 +20,5 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='./user_profile_avatar')
     hobbies = models.CharField(max_length=255, blank=True)
     favorite_animals = models.CharField(max_length=255, blank=True)
-    user = models.OneToOneField(User, related_name='profile')
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
 
